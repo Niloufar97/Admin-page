b2f664ca9fce0a8ab4645cf3e3bfbb256d5a9d0f
Niloufar <nilooooofar97@gmail.com>
2024-09-29 18:48:11 +0330

    fix style

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LineChartComponent } from "./components/line-chart/line-chart.component";
+import { CardsComponent } from "./components/cards/cards.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [LineChartComponent],
+  imports: [LineChartComponent, CardsComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
