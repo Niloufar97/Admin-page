a564013782b8cfd53d226e65a03105c7a55f1a6e
Niloufar <nilooooofar97@gmail.com>
2024-10-02 17:56:58 +0330

    first setup for charts

--- src/app/pages/dashboard/components/cards/cards.component.ts
+++ src/app/pages/dashboard/components/cards/cards.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 @Component({
   selector: 'app-cards',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,CommonModule],
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.css'
 })
 export class CardsComponent {
-
+  cards = [
+    { title: 'Card 1', content: 'Content of card 1' },
+    { title: 'Card 2', content: 'Content of card 2' },
+    { title: 'Card 3', content: 'Content of card 3' },
+    { title: 'Card 4', content: 'Content of card 4' },
+    { title: 'Card 5', content: 'Content of card 5' },
+  ];
 }
