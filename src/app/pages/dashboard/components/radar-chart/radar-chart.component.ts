d6230297d4d107a75e6ca995161de14ea6a6d84c
Niloufar <nilooooofar97@gmail.com>
2024-10-18 11:10:41 +0200

    add radar chart

---
+++ src/app/pages/dashboard/components/radar-chart/radar-chart.component.ts
@@ -0,0 +1,68 @@
+import { Component, ViewChild, OnDestroy } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+
+@Component({
+  selector: 'app-radar-chart',
+  templateUrl: './radar-chart.component.html',
+  styleUrls: ['./radar-chart.component.scss'],
+  standalone: true,
+  imports: [BaseChartDirective],
+})
+export class RadarChartComponent implements OnDestroy {
+  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
+
+  // Radar
+  public radarChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    maintainAspectRatio: false,
+  };
+
+  public radarChartLabels: string[] = [
+    'Sales Volume',
+    'Revenue Growth',
+    'Customer Acquisition',
+    'Average Order Value',
+    'Discount Rate',
+    'Return Rate',
+    'Market Share',
+  ];
+
+  public radarChartData: ChartData<'radar'> = {
+    labels: this.radarChartLabels,
+    datasets: [
+      { data: [65, 59, 90, 81, 56, 55, 40], label: '2022' },
+      { data: [28, 48, 40, 19, 96, 27, 100], label: '2023' },
+      { data: [75, 68, 60, 13, 75, 20, 80], label: '2024' },
+    ],
+  };
+  public radarChartType: ChartType = 'radar';
+
+  ngOnDestroy(): void {
+    // Destroy the chart when the component is destroyed to prevent memory leaks
+    if (this.chart) {
+      this.chart.chart?.destroy();
+    }
+  }
+
+  // events
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event: ChartEvent;
+    active: object[];
+  }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event: ChartEvent;
+    active: object[];
+  }): void {
+    console.log(event, active);
+  }
+}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LineChartComponent } from "./components/line-chart/line-chart.component";
 import { CardsComponent } from "./components/cards/cards.component";
+import { RadarChartComponent } from "./components/radar-chart/radar-chart.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [LineChartComponent, CardsComponent],
+  imports: [LineChartComponent, CardsComponent, RadarChartComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
